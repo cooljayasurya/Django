6324226fd710c34400371ddf75fad27c22c86f4f
Benseron-16 <jayasuryamkm@gmail.com>
2019-06-23 12:11:44 +0530

    url change

--- src/src/urls.py
+++ src/src/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from customer import views
-from ..controller.Router import Router
+from controller.Router import Router
 
 urlpatterns = [
     path('', views.welcome_view, name='home'),
     path('demo', views.signUpCustomer),
-    path('admin/', admin.site.urls),
-    path('', Router.routerConfig)
+    path('admin/', admin.site.urls)
 ]
